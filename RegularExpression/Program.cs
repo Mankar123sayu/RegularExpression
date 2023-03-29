89cf6159531bca48559bfb4b22d1a76b88b4fe25
Mankar123sayu <sayumankar77@gmail.com>
2023-03-29 15:57:48 +0530

    Regular

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegularExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            Validate code = new Validate();
+            code.Try("400088");
+        }
+    }
+}
---
+++ RegularExpression/Validation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class Validate
+    {
+        public const string PIN_CODE = "^[0-9]{6}$";
+        public void Try(string pinCode)
+        {
+            if (Regex.IsMatch(pinCode, PIN_CODE))
+            {
+                Console.WriteLine("Validation Successfull");
+            }
+            else
+            {
+                Console.WriteLine("Validation UnSuccessfull");
+            }
+        }
+    }
+}
